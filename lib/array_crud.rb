b1a3995432c88e2bbe164cb8c7e04be9bd205fef
Lialah Barrer <lrbarrer@gmail.com>
2019-09-03 22:12:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["happy", "sad", "angry", "nervous"]
+  emotions = ["happy", "sad", "angry", "nervous"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  emotions << "anxiety"
 end
 
 def add_element_to_start_of_array(array, element)
