693a45d3e11d49acac4720946a907564e5ac85ff
Lialah Barrer <lrbarrer@gmail.com>
2019-09-03 22:14:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  [] << "anxiety"
+  ["happy", "sad", "angry", "nervous"] << "anxiety"
 end
 
 def add_element_to_start_of_array(array, element)
